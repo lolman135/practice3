f307b0039705de4ed7b9b34778deb91add3f092c
Kyrylo Kusovsky <kkirilll2006@gmail.com>
2024-10-12 08:03:24 +0300

    Add BorderDecorator

---
+++ src/app/BorderDecorator.java
@@ -0,0 +1,22 @@
+package app;
+
+public class BorderDecorator extends FigureDecorator{
+
+    private int borderWidth;
+
+    public BorderDecorator(Figure decoratedFigure, int borderWidth) {
+        super(decoratedFigure);
+        this.borderWidth = borderWidth;
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription() + "with " + borderWidth + "px. width border";
+    }
+
+    @Override
+    public void draw(int x, int y) {
+        System.out.printf("Draw border, width: %dpx.", borderWidth);
+        super.draw(x, y);
+    }
+}
